b5c8f8c4cf3fe90898227a6358b15142bc6aa8a3
Arne <six.arne@gmail.com>
2020-03-17 15:53:54 +0100

    added sensory data

--- sense_dashboard/app/scripts/app.js
+++ sense_dashboard/app/scripts/app.js
@@ -1,6 +1,6 @@
 const dbConfig = {
-    collection: 'raspberry_collection',
-    document: 'lectorpi_doc'
+    collection: 'raspberry',
+    document: 'dashboard'
 };
 
 const app = {
@@ -11,20 +11,62 @@ const app = {
         // cache the DOM
         this.cacheDOMElements();
         this.cacheDOMEvents();
+
+        this.humidity = document.querySelector('.humidity');
+        this.compass = document.querySelector('.compass');
+        this.temp = document.querySelector('.temp');
+
+        this.Aroll = document.querySelector('.a-roll');
+        this.Apitch = document.querySelector('.a-pitch');
+        this.Ayaw = document.querySelector('.a-yaw');
+
+        this.Groll = document.querySelector('.g-roll');
+        this.Gpitch = document.querySelector('.g-pitch');
+        this.Gyaw = document.querySelector('.g-yaw');
+
+        this._matrix = {
+            isOn: false, color: {value: '#000000', type: 'hex'}
+        };
     },
     cacheDOMElements() {
         this.$colorPicker = document.querySelector('#colorPicker');
+        this.$toggleMatrix = document.querySelector('#toggleMatrix');
         this.$btnChange = document.querySelector('#btnChange');
     },
     cacheDOMEvents() {
         this.$btnChange.addEventListener('click', (e) => {
             e.preventDefault();
 
-            const color = this.$colorPicker.value;
+            this._matrix.color.value = this.$colorPicker.value;
+            this._matrix.isOn = this.$toggleMatrix.checked;
+
+            this.updateInFirebase();
+        });
+    },
+    cacheFirestoreChanges() {
+        this._db.collection(dbConfig.collection).doc(dbConfig.document).onSnapShot((snap) => {
+            const data = snap.data();
+
+            this.humidity.innerText = data.humidity;
+            this.temp.innerText = data.temp;
+            this.compass.innerText = data.compass;
+            
+            this.Aroll.innerText = data.accelero.roll;
+            this.Apitch.innerText = data.accelero.pitch;
+            this.Ayaw.innerText = data.accelero.yaw;
+
+            this.Groll.innerText = data.gyro.roll;
+            this.Gpitch.innerText = data.gyro.pitch;
+            this.Gyaw.innerText = data.gyro.yaw;
+            
         });
     },
     updateInFirebase() {
-        
+        this._db.collection(dbConfig.collection).doc(dbConfig.document)
+            .set(
+                {matrix: this._matrix},
+                {merge: true}
+            );
     }
 }
 
