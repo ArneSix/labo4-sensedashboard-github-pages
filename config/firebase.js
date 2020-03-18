97ffbc26e68b286102c0389e6cd11f50f88a5bf3
Arne <six.arne@gmail.com>
2020-03-18 12:10:32 +0100

    added docs folder for github page

---
+++ config/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCT3eZYrhnyjxX86H-2QHnLixaEfjkTI1I",
+    authDomain: "my-project-1525178706735.firebaseapp.com",
+    databaseURL: "https://my-project-1525178706735.firebaseio.com",
+    projectId: "my-project-1525178706735",
+    storageBucket: "my-project-1525178706735.appspot.com",
+    messagingSenderId: "1007638993235",
+    appId: "1:1007638993235:web:63c5874dd4cfb297c569a6"
+};
